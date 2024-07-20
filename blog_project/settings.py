1f7f3db1280a2debdb8c89df40d896b653c194a1
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-20 19:10:14 +0530

    wsgi update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -113,9 +113,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
