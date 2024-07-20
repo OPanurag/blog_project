6bce878acb6adb24bfbab812cbe55d271f7b5702
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-20 21:49:43 +0530

    aasd

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'content', 'created_at')
+    search_fields = ('content',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
