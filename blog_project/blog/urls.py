99b14080fa864edc492c6d7e4ecf7d2e71287859
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-21 00:26:36 +0530

    123

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import PostListCreateView, PostRetrieveUpdateDestroyView, CommentListCreateView, home, post_detail, login
+from django.urls import path
+from . import views
+from .views import PostListCreateView, PostRetrieveUpdateDestroyView, CommentListCreateView, home, post_detail
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
@@ -8,6 +9,19 @@ urlpatterns = [
     path('posts/<int:post_pk>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Change to JWT view
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', home, name='home'),
     path('post/<int:pk>/', post_detail, name='post-detail'),
+    path('', views.home, name='home'),
+
+    path('create/', views.create_post, name='create_post'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('api/posts/', views.PostListCreateView.as_view(), name='api_post_list_create'),
+    path('api/posts/<int:pk>/', views.PostRetrieveUpdateDestroyView.as_view(), name='api_post_detail'),
+    path('api/posts/<int:post_pk>/comments/', views.CommentListCreateView.as_view(), name='api_comment_list_create'),
+    path('api/login/', views.login, name='api_login'),
+
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,10 @@
+# views.py
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -7,14 +12,51 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .forms import PostForm
-from .models import Comment
-from .models import Post
+from .models import Comment, Post
 from .serializers import PostSerializer, CommentSerializer
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')  # Redirect to home page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after sign up
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
+
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
+    return Response({'error': 'Invalid credentials'}, status=400)
 
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {'posts': posts})
+
 
 @login_required
 def create_post(request):
@@ -28,14 +70,11 @@ def create_post(request):
     else:
         form = PostForm()
     return render(request, 'blog/create_post.html', {'form': form})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
 
 
 class PostListCreateView(generics.ListCreateAPIView):
@@ -67,17 +106,3 @@ class CommentListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         post = Post.objects.get(pk=self.kwargs['post_pk'])
         serializer.save(post=post, author=self.request.user)
-
-
-@api_view(['POST'])
-def login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        })
-    return Response({'error': 'Invalid credentials'}, status=400)
