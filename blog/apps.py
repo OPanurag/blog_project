46bb81dbcc6a5e6418ed503784c851630ec9a02f
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-20 23:01:11 +0530

    a

--- blog/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class BlogConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "blog"
-
-    def ready(self):
-        # This method is called when Django starts up
-        print("Blog app is ready!")
