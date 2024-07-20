53ad2b84a56d89470597521ca42c68e0ccb6c16f
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-20 23:50:31 +0530

    asd

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListCreateView, PostRetrieveUpdateDestroyView, CommentListCreateView, home, post_detail
+from .views import PostListCreateView, PostRetrieveUpdateDestroyView, CommentListCreateView, home, post_detail, login
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
     path('posts/<int:post_pk>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('login/', login, name='login'),  # Add this line
     path('', home, name='home'),
     path('post/<int:pk>/', post_detail, name='post-detail'),
 ]
