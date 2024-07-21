7aa4df0f0f42c57bc5eecbe52144845186567b9e
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-21 23:04:01 +0530

    Update urls.py

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,9 +1,14 @@
 # blog_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Include the blog app URLs
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
